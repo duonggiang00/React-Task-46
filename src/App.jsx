803a6acd3aa62b42606702b95d12562fc619aeb7
duonggiang00 <duonggiang00@gmail.com>
2025-05-27 17:23:53 +0700

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ProductList from './ProductList'
+import "./App.css";
+import AppRouter from "./router";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <ProductList />
-    </>
-  )
+	return<>
+		<AppRouter />;
+	</>
+	 
 }
 
-export default App
+export default App;
